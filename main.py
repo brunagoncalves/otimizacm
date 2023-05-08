c282c66a53f49ce61dc49a4d5d04482b6d52fc69
Bruna Goncalves <brunagoncalves.sjb@gmail.com>
2023-05-08 17:57:18 -0300

    settings

--- main.py
+++ main.py
@@ -1,186 +1,160 @@
 import os
-from tkinter import Tk, filedialog, ttk, Label, Button, Entry, PhotoImage
+from tkinter import *
+from tkinter import filedialog, ttk, font
 from PIL import Image
 
 
-def select_path_images():
-    global path_images
-    path_images = filedialog.askdirectory()
-    label_path_images.config(text=path_images)
+def select_path_image():
+    global path_folder_image
+    path_folder_image = filedialog.askdirectory()
+    label_path_folder_image.config(text=path_folder_image)
 
 
 def select_path_save():
-    global path_save
-    path_save = filedialog.askdirectory()
-    label_path_save.config(text=path_save)
+    global path_folder_save
+    path_folder_save = filedialog.askdirectory()
+    label_path_folder_save.config(text=path_folder_save)
 
 
-def resize_images():
-    global width
-    global height
+def resize_image():
     width = int(width_entry.get())
     height = int(height_entry.get())
 
-    files = os.listdir(path_images)
+    files = os.listdir(path_folder_image)
     total_files = len(files)
 
-    progress_label.config(text=f"Redimensionando imagens...")
+    progress_label.config(text=f'Redimensionando imagens...')
     progress_bar.config(maximum=total_files)
 
     for image, file in enumerate(files):
         if file.endswith(".png"):
-            images = Image.open(os.path.join(path_images, file))
+            images = Image.open(os.path.join(path_folder_image, file))
             images = images.resize((width, height))
             images.convert('RGB').save(os.path.join(
-                path_save, file.replace(".png", ".jpg")))
+                path_folder_save, file.replace(".png", ".jpg")))
         progress_bar.step(1)
         progress_label.config(
-            text=f"Redimensionando imagem {image+1}/{total_files}")
+            text=f'Redimensionando imagem {image+1}/{total_files}')
         window.update()
 
-    progress_label.config(text=f"Redimensionamento concluído!")
+    progress_label.config(text=f'Redimensionamento concluído!')
 
 
 # Create screen
 window = Tk()
+window.iconphoto(False, PhotoImage(file='./icons/icon-app.png'))
 window.title('CM Otimiza - BETA')
-window.geometry("600x600")
+window.geometry('500x500')
+window.resizable(False, False)
 
-# Define icons
-folder_open = PhotoImage(file='./icons/folder-open.png')
+# Variables
+ipadding = {'ipadx': 3, 'ipady': 3}
+padd_button = {'padx': 50, 'pady': 5}
 
-# Button select path
+# Fonts
+font_bold = font.Font(
+    size=9,
+    weight='bold'
+)
+
+# Icons
+icon_folder = PhotoImage(file='./icons/folder-open.png')
+icon_exit = PhotoImage(file='./icons/exit.png')
+icon_resize = PhotoImage(file='./icons/resize.png')
+
+# Select folder image
 btn_path_image = Button(
     window,
-    image=folder_open,
-    compound='center',
-    command=select_path_images
+    image=icon_folder,
+    text='  O que você quer redimensionar?',
+    compound='left',
+    font=font_bold,
+    command=select_path_image
 )
 
-btn_path_image.grid(
-    row=0,
-    column=0,
-    padx=5,
-    pady=3,
-    ipadx=3,
-    ipady=3
-)
+btn_path_image.pack(**ipadding, padx=50, pady=(15, 5), fill='x')
 
-btn_path_save = Button(
+# Label folder images
+label_path_folder_image = Label(
     window,
-    image=folder_open,
-    compound='center',
+    text='',
+    bg='white',
+    fg='green',
+    font=font_bold
+)
+
+label_path_folder_image.pack(**ipadding, **padd_button, fill='x')
+
+# Select folder save
+btn_path_image = Button(
+    window,
+    image=icon_folder,
+    text='  Onde deseja salvar?',
+    compound='left',
+    font=font_bold,
     command=select_path_save
 )
 
-btn_path_save.grid(
-    row=2,
-    column=0,
-    padx=5,
-    pady=3,
-    ipadx=3,
-    ipady=3
-)
+btn_path_image.pack(**ipadding, **padd_button, fill='x')
 
-# Label select path
-label_path_images = Label(
+# Label folder save
+label_path_folder_save = Label(
     window,
-    text=''
+    text='',
+    bg='white',
+    fg='green',
+    font=font_bold
 )
 
-label_path_images.grid(
-    row=0,
-    column=1,
-    padx=10,
-    pady=5
-)
+label_path_folder_save.pack(**ipadding, **padd_button, fill='x')
 
-label_path_save = Label(
-    window,
-    text=''
-)
+# Label size
+width_label = Label(window, text='Largura')
+width_label.pack(**ipadding, **padd_button)
 
-label_path_save.grid(
-    row=2,
-    column=1,
-    padx=10,
-    pady=5
-)
+# Entry size
+width_entry = Entry(window, font=font_bold, justify='center')
+width_entry.pack(**ipadding, **padd_button)
 
-# Entry Width - Height
-width_label = Label(window, text="Largura:")
+# Label size
+height_label = Label(window, text='Altura')
+height_label.pack(**ipadding, **padd_button)
 
-width_label.grid(
-    row=3,
-    column=1
-)
-
-width_entry = Entry(window)
-
-width_entry.grid(
-    row=3,
-    column=2
-)
-
-height_label = Label(window, text="Altura:")
-
-height_label.grid(
-    row=4,
-    column=1
-)
-
-height_entry = Entry(window)
-
-height_entry.grid(
-    row=4,
-    column=2
-)
+# Entry size
+height_entry = Entry(window, font=font_bold, justify='center')
+height_entry.pack(**ipadding, **padd_button)
 
 # Button resize
 btn_resize = Button(
     window,
-    text='Redimensionar Imagens',
-    command=resize_images
+    image=icon_resize,
+    text='  Redimensionar',
+    font=font_bold,
+    compound='left',
+    command=resize_image
 )
 
-btn_resize.grid(
-    row=5,
-    column=0,
-    padx=5,
-    pady=3,
-    ipadx=3,
-    ipady=3
-)
+btn_resize.pack(**ipadding, **padd_button, fill='x')
 
-# BarProgress
-progress_bar = ttk.Progressbar(
-    window,
-    orient="horizontal",
-    length=300,
-    mode="determinate"
-)
+# Progress Bar
+progress_bar = ttk.Progressbar(window, orient='horizontal')
+progress_bar.pack(**padd_button, fill='x')
 
-progress_bar.grid(
-    row=6
-)
-
-progress_label = Label(
-    window,
-    text=""
-)
-
-progress_label.grid(
-    row=7
-)
+# Progress label
+progress_label = Label(window, text='')
+progress_label.pack(**ipadding, **padd_button)
 
 # Button exit
 btn_exit = Button(
     window,
-    text='Sair',
-    command=window.quit
-)
+    image=icon_exit,
+    text='  Fechar',
+    compound='left',
+    font=font_bold,
+    fg='red',
+    command=window.quit)
 
-btn_exit.grid()
+btn_exit.pack(**ipadding, **padd_button)
 
-# Start screen
+# Start window
 window.mainloop()
